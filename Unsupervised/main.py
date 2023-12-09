887bca9881a15f071e88d5ee819a21119887b381
Aryan Singh <aryan_singh@brown.edu>
2023-12-09 04:44:33 -0500

    Added clustering via kmeans

--- Unsupervised/main.py
+++ Unsupervised/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.regression.rolling import RollingOLS
 import datetime as dt
+from sklearn.cluster import KMeans
 import yfinance as yf
 import pandas_ta
 from indicators import *
@@ -60,11 +61,40 @@ def calculate_rolling_basis(factor_data):
     factor_data = factor_data.groupby('ticker').shift()
     return factor_data
     
+def make_clusters(data):
+    initial_centroids = initialize_centroids()
+    data['cluster'] = KMeans(n_clusters=4, random_state=0,init=initial_centroids).fit(data).labels_
+    return data
+
+def plot_clusters(data):
+    cluster_0 = data[data['cluster'] == 0]
+    cluster_1 = data[data['cluster'] == 1]
+    cluster_2 = data[data['cluster'] == 2]
+    cluster_3 = data[data['cluster'] == 3]
+    
+    plt.scatter(cluster_0.iloc[:,0], cluster_0.iloc[:,6], color='red', label='Cluster 0')
+    plt.scatter(cluster_1.iloc[:,0], cluster_1.iloc[:,6], color='green', label='Cluster 1')
+    plt.scatter(cluster_2.iloc[:,0], cluster_2.iloc[:,6], color='blue', label='Cluster 2')
+    plt.scatter(cluster_3.iloc[:,0], cluster_3.iloc[:,6], color='black', label='Cluster 3')
+    
+    plt.legend()
+    plt.show()
+    return
+
+
+# Make clusters consistent across all rsi values (i.e. cluster 3 always assigned to 55 <= rsi)
+def initialize_centroids():
+    target_rsi_values = [30, 45, 55, 70]
+    initial_centroids = np.zeros((len(target_rsi_values), 18))
+    initial_centroids[:, 6] = target_rsi_values
+    return initial_centroids
+
 def main():
     sp500_df = get_SP500()
-    sp500_df = calculate_simple_indicators(sp500_df=sp500_df)
-    sp500_df['atr'] = sp500_df.groupby(level=1, group_keys=False).apply(calculate_atr)
-    sp500_df['macd'] = sp500_df.groupby(level=1, group_keys=False)['adj close'].apply(calculate_macd)
+    indicator_calculator = IndicatorCalculator(sp500_df=sp500_df)
+    sp500_df = indicator_calculator.calculate_simple_indicators(sp500_df=sp500_df)
+    # sp500_df['atr'] = sp500_df.groupby(level=1, group_keys=False).apply(calculate_atr)
+    # sp500_df['macd'] = sp500_df.groupby(level=1, group_keys=False)['adj close'].apply(calculate_macd)
     aggregate_data = aggregate(sp500_df=sp500_df)
     aggregate_data = filter_150(aggregate_data)
     aggregate_data = aggregate_data.groupby(level=1,group_keys=False).apply(calculate_returns).dropna()
@@ -72,6 +102,7 @@ def main():
     # print(aggregate_data)
     factor_data = get_fama_french_factors()
     factor_data = factor_data.join(aggregate_data['return_1m']).sort_index()
+    
     
     # Filter out stocks with less than 10 months of data
     observations = factor_data.groupby(level=1).size()
@@ -85,8 +116,14 @@ def main():
     aggregate_data.loc[:, factors] = aggregate_data.groupby('ticker', group_keys=False)[factors].apply(lambda x: x.fillna(x.mean()))
     aggregate_data = aggregate_data.drop('adj close', axis=1)
     aggregate_data = aggregate_data.dropna()
-    print(aggregate_data.info())
     
+    aggregate_data = aggregate_data.dropna().groupby('date', group_keys=False).apply(make_clusters)
+    
+    plt.style.use('ggplot')
+    for i in aggregate_data.index.get_level_values('date').unique().tolist():
+        level = aggregate_data.xs(i, level=0)
+        plt.title(f'Date {i}')
+        plot_clusters(level)
     
 if __name__ == "__main__":
     main()
